af068514edb9e6908d749706ee85c14a786374f5
Julianne Kim <juliannne@gmail.com>
2017-11-06 16:11:34 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,22 +24,24 @@ class Pokemon
     @id = results.flatten.first
   end
 
-  def self.new_from_db(row)
-    self.new.tap do |post|
-      post.name = row[1]
-      post.type = row[2]
-      post.id = row[0]
-      post.hp = row[3]
-    end
-  end
+  # def self.new_from_db(row)
+  #   self.new.tap do |post|
+  #     post.name = row[1]
+  #     post.type = row[2]
+  #     post.id = row[0]
+  #     post.hp = row[3]
+  #   end
+  # end
 
   def self.find(id, db)
-    sql = <<-SQL
-      SELECT * FROM pokemon WHERE id = ? LIMIT 1
-    SQL
-
-    results = db.execute(sql, id).flatten
-    Pokemon.new_from_db(results)
+    # sql = <<-SQL
+    #   SELECT * FROM pokemon WHERE id = ? LIMIT 1
+    # SQL
+    # 
+    # results = db.execute(sql, id).flatten
+    # Pokemon.new_from_db(results)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
   def alter_hp(hp, db)
